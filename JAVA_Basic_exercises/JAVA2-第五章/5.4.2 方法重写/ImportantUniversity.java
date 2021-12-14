5a73483c1c0e17b532d38747232d69232c1287b7
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2021-12-14 13:33:43 +0800

    Create ImportantUniversity.java

---
+++ JAVA_Basic_exercises/JAVA2-第五章/5.4.2 方法重写/ImportantUniversity.java
@@ -0,0 +1,9 @@
+public class ImportantUniversity extends University{
+    void enterRule(double math,double english, double chinese){
+        double total = math + english + chinese;
+        if(total >220)
+            System.out.println(total + "分数达到重点大学录取线");
+        else 
+            System.out.println(total + "分数未达到重点大学的录取线");
+    }
+}
