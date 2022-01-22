70e811abb72fccaef88c5ea9f71747bb33666975
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-01-22 21:14:32 +0800

    Create sin_cos_tan.py

---
+++ Python_NumPy/ch3/sin_cos_tan.py
@@ -0,0 +1,10 @@
+# 三角函数 ，接下来对基本的正弦 余弦 正切 及其反函数 还有 换算成弧度制度函数
+np.sin(0)
+np.cos(0)
+np.tan(0)
+np.sin(np.pi*0.5)
+np.cos(np.pi*0.5) # 结果应当是0 cos90° = 0
+np.tan(np.pi*0.5) # 无限发散的值  tan90° = 1 / 0 
+np.sin(1)
+np.cos(1)
+np.tan(1)
