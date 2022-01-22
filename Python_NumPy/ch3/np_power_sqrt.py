f66129c8de5183e0be05396cbe85666f43afbf26
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-01-22 21:04:43 +0800

    Create np_power_sqrt.py

---
+++ Python_NumPy/ch3/np_power_sqrt.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = np.array([0,1,2,3,4])
+b = np.array([2,4,6,8,10])
+# 求 幂运算 和 求平方根运算
+np.power(2,3)
+2**3
+a = np.arange(1,11,1)
+a
+b  = np.array([1,2,1,2,1,2,1,2,1,2])
+b
+np.power(a,b)
+a**b
+np.sqrt(2) # 求根号
+2**0.5
+np.sqrt(a)
