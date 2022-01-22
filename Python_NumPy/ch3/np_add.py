d0f4dc1e1400b298b859743811d8a2fa8df7fcb9
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-01-22 20:33:28 +0800

    Update np_add.py

--- Python_NumPy/ch3/np_add.py
+++ Python_NumPy/ch3/np_add.py
@@ -2,8 +2,8 @@ import numpy as np
 a = np.array([0,1,2,3,4])
 b = np.array([2,4,6,8,10])
 
-a + b # OUT 1
-a + 4 # OUT 3
+print (a + b) # OUT 1
+print(a + 4) # OUT 3
 # 使用 np.add()也是同样效果
-np.add(a,b)# OUT 3
-np.add(a,4) # # OUT 4
+print (np.add(a,b))# OUT 3
+print (np.add(a,4)) # # OUT 4
