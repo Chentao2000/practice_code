50d8d8c194d9ec0634af2c0b2ba6b04693c8f3bc
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-01-22 20:37:08 +0800

    Update and rename np_add.py to np_add_subtract.py

--- Python_NumPy/ch3/np_add_subtract.py
+++ Python_NumPy/ch3/np_add_subtract.py
@@ -6,4 +6,9 @@ print (a + b) # OUT 1
 print(a + 4) # OUT 3
 # 使用 np.add()也是同样效果
 print (np.add(a,b))# OUT 3
-print (np.add(a,4)) # # OUT 4
+print (np.add(a,4))  # OUT 4
+print (a - b)# # OUT 5
+print( b- a)# # OUT 6
+print (a - 4)# # OUT 7
+print(np.subtract(a,b))# # OUT 8
+print(np.subtract(a,4))# # OUT 9
