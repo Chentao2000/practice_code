17158106f381818a916adb73c8d763f366dfeba7
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-01-25 12:28:39 +0800

    Create sinhx_coshx_tanhx.py

---
+++ Python_NumPy/ch3/sinhx_coshx_tanhx.py
@@ -0,0 +1,17 @@
+# 双曲函数 
+# sinhx = (e^x^ - e^x^ ) /2
+# coshx = (e^x^ + e^x^ ) /2
+# tanhx = sinhx / coshx
+# numpy 中也有这些函数
+import numpy as np
+np.sinh(2)
+np.cosh(2)
+np.tanh(2)
+
+np.sinh(-1)
+np.cosh(-1)
+np.tanh(-1)
+
+np.arcsin(1)
+np.arccosh(1)
+np.arctan(0.7)
