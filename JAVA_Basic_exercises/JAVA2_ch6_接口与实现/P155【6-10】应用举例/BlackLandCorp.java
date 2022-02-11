38f7f47d679d0fc5f370717e9f6c959293a2cec6
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-02-11 16:22:14 +0800

    Create BlackLandCorp.java

---
+++ JAVA_Basic_exercises/JAVA2_ch6_接口与实现/P155【6-10】应用举例/BlackLandCorp.java
@@ -0,0 +1,12 @@
+
+public class BlackLandCorp implements Advertisement{
+    public void showAdvertisement(){
+        System.out.println("*******************");
+        System.out.println("劳动是爹 \n 土地是妈 \n");
+        System.out.println("********************");
+        
+    }
+    public String getCorpName(){
+        return "黑土集团";
+    }
+}
