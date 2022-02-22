f6d39bee42f0a3280582e316d262f9b8247e2843
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-02-22 11:35:25 +0800

    Create P182【8-1-3】String类的常用方法[7-8].java

---
+++ JAVA_Basic_exercises/JAVA2_ch8_常用实用类/P182【8-1-3】String类的常用方法[7-8].java
@@ -0,0 +1,27 @@
+/*
+- --- 7
+* public Sting substring (int startpoint)
+
+    -- 字符串对象调用该方法获得一个新的String 对象 ，新的String对象的字符序列是复杂当前
+        String 对象的字符序列中的Startpoint位置上字符所得到字符序列，
+        String 对象调用subString(int start , int end)的方法获得一个新的String对象
+        新的String 对象 的字符序列是赋值String对象 ，字符串序列中的 start 位置 至 end -1 所得的序列
+        String tom  = "我喜欢篮球";
+            
+            String 对象 str 的字符序列是 "喜欢"; (注意，不是喜欢：“篮”) 
+- --- 8   
+    
+    public String trim()
+
+    * string 对象调用方法trim() 得到一个新的 String对象，这个新的 String对象 的字符序列是
+    当前String对象的字符串序列去掉前后空格序列
+   * 
+*/
+
+public class prog
+{
+	public static void main(String[] args) {   
+    
+		
+	}
+}
