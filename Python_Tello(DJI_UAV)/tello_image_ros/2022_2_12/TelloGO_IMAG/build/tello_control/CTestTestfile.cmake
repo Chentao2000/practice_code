22bcbee4349bba1c51437df2a34f367bc50b5f2a
Your Name <Chentao2000@github.com>
2022-03-05 11:54:20 +0800

    修改了bug

--- Python_Tello(DJI_UAV)/tello_image_ros/2022_2_12/TelloGO_IMAG/build/tello_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tao/Desktop/TelloGO_IMAG/src/tello_control
-# Build directory: /home/tao/Desktop/TelloGO_IMAG/build/tello_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
