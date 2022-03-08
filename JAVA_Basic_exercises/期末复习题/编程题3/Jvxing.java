ffb996b8a78b8b76e2ff23638d6af2f8cde24589
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-03-09 01:20:48 +0800

    Create Jvxing.java

---
+++ JAVA_Basic_exercises/期末复习题/编程题3/Jvxing.java
@@ -0,0 +1,25 @@
+public class Jvxing
+{
+    //成员变量
+    private double width;
+    private double chang;
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getChang() {
+        return chang;
+    }
+    public void setChang(double height) {
+        this.chang = height;
+    }
+    
+    
+    //成员方法
+    public double mianji()
+    {
+        return this.width*this.chang;
+    }
+}
