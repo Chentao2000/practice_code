d7d93bfdc8a30d0042a08317da5aa005c656c9a2
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-03-21 21:21:43 +0800

    Add files via upload

---
+++ C++_ORCA/DepthBlue/Path_planning/ch2/Stu_random_complex_generator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/search/impl/kdtree.hpp>
+
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <nav_msgs/Odometry.h>
+#include <Eigen/Eigen>
+#include <math.h>
+#include <random>
+
+
+using namespace std;
+using namespace Eigen;
+
+ros::Publisher _all_map_pub;
+int  _obs_num,_cir_num;
+double _x_size, _y_size, _z_size, _init_x, _init_y, _resolution, _sense_rate;
+double _x_l, _x_h, _y_l, _y_h, _w_l, _w_h, _h_l, _h_h, _w_c_l, _w_c_h;
