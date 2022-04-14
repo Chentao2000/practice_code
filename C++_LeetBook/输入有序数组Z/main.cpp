f2185b151d95c19fadcc36aa7d182db73545117c
陈涛 <68007558+Chentao2000@users.noreply.github.com>
2022-04-14 21:36:53 +0800

    Create main.cpp

---
+++ C++_LeetBook/输入有序数组Z/main.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int head = 0,tail = numbers.size() - 1;
+        while(head < tail)
+        {
+            if(numbers[head] + numbers[tail] < target)
+            {
+                head++;
+            }
+            else if(numbers[head] + numbers[tail] > target)
+            {
+                tail--;
+            }
+            else
+            {
+                return {head+1, tail+1};
+            }
+        }
+        return {0,0};
+    }
+};
